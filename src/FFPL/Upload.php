7a1fc804c04f6be22f83407fb1730169334b7706
Felipe Prestes <the.copier.kakashi@gmail.com>
2015-08-06 19:48:41 -0400

    Removed permissions verification from Upload.php

--- src/FFPL/Upload.php
+++ src/FFPL/Upload.php
@@ -9,12 +9,12 @@ namespace FFPL {
 			@$perms = fileperms($output);
 			//need owner or group write permissions to output folder (same owner or group
 			// from server)
-			if (is_dir($output) && ((($perms & 0x0080) && (fileowner($output) == posix_geteuid())) || (($perms & 0x0010) && (filegroup($output) == posix_getegid())))) :
+			//if (is_dir($output) && ((($perms & 0x0080) && (fileowner($output) == posix_geteuid())) || (($perms & 0x0010) && (filegroup($output) == posix_getegid())))) :
 				if (move_uploaded_file($file["tmp_name"], $fileOutput)) :
 					chmod($fileOutput, 0666);
 					return array('oldFileName' => $file['name'], 'outputFileName' => $newFileName, 'extension' => $fileExtension, 'size' => filesize($fileOutput));
 				endif;
-			endif;
+			//endif;
 			return false;
 		}
 
